17a3ea620150865f3321c5b29bc8def4c637e0e9
Mahmoud Ayman <mahmoud_aymann@outlook.com>
2018-12-20 02:17:39 +0200

    home

--- app/src/main/java/com/tkmsoft/akarwebview/MainActivity.java
+++ app/src/main/java/com/tkmsoft/akarwebview/MainActivity.java
@@ -1,18 +1,52 @@
 package com.tkmsoft.akarwebview;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String URL = "http://3qaronline.net/admin/auth";
+    private WebView webView;
+    @BindView(R.id.toolbarTV)
+    TextView toolbarTV;
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         Toast.makeText(this, "loading,,,", Toast.LENGTH_SHORT).show();
-        WebView webView = findViewById(R.id.webView);
-        webView.loadUrl("http://3qaronline.net/admin/auth");
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(URL);
+
+        toolbarTV.setText(getString(R.string.app_name));
+    }
+
+    @OnClick(R.id.toolbarIB)
+    protected void onToolbarIBOnClick(){
+        webView.reload();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
